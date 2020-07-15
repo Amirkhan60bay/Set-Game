dae0961d5bd8ae97126f2de11f674bbaead86987
amirkhan <amirkhan60bay@gmail.com>
2020-07-15 23:08:37 +0800

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Amirkhan Alpysabayev on 7/15/20.
+//  Copyright © 2020 Amirkhan Alpyspayev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
